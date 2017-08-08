0f286e2a869e6f0da0857667c3a9668456ac640c
Luisa Scavo <luisascavo@gmail.com>
2017-08-08 11:40:37 -0400

    update family model to use bcrypt

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,5 +1,3 @@
 class Family < ActiveRecord::Base
-  validates :family_code_digest, uniqueness: true
-
-  before_save :generate_family_code
+  has_secure_password
 end
--- db/migrate/20170807221940_create_families.rb
+++ db/migrate/20170807221940_create_families.rb
@@ -2,7 +2,7 @@ class CreateFamilies < ActiveRecord::Migration
   def change
     create_table :families do |t|
       t.string :name
-      t.string :family_code_digest
+      t.string :password_digest
 
       t.timestamps null: false
     end
