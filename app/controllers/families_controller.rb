882a4bfda4ec0467daeb9acacbcc3d2b74dbb445
Luisa Scavo <luisascavo@gmail.com>
2017-08-08 15:36:31 -0400

    sketch out families controller, create new family form

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,15 @@
 class FamiliesController < ApplicationController
+  def new
+    @family = Family.new
+  end
+
+  def create
+    @family = Family.create(family_params)
+  end
+
+  private
+
+  def family_params
+    params.require(:family).permit(:name, :password, :password_confirmation, :users[:name, :password, :password_confirmation])
+  end
 end
