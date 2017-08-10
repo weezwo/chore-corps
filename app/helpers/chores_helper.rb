e39b13fd7f51069af8202b221cd9a711bc4d7309
Luisa Scavo <luisascavo@gmail.com>
2017-08-10 15:07:02 -0400

    stub out claimed_chore?, add comment for magic numbers

--- app/helpers/chores_helper.rb
+++ app/helpers/chores_helper.rb
@@ -1,10 +1,14 @@
 module ChoresHelper
   def cycle_opts
     [["Daily", "86400"], ["Weekly","604800"], ["Monthly","2592000"], ["Seasonally", "7776000"], ["Yearly","31557600"]]
-  end ## Numbers correspond to the number of seconds that should elapse between 
+  end ## Numbers correspond to the number of seconds that should elapse between tasks
 
   def chore_frequency
     pluralize @chore.cycle / 86400, "day"
+  end
+
+  def claimed_chore?
+    @chore.tasks.any?{|task| task.completion_status.nil?}
   end
 
   def claimed_chore_notice
