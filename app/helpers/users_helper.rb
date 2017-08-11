aba3fc678b42d1dd6fcc83582e09e334cb9e3cbd
Luisa Scavo <luisascavo@gmail.com>
2017-08-11 11:29:01 -0400

    begin implement #poach_or_complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def poach_or_complete
+    if @user == current_user
+      form_tag url_for(:controller => 'tasks', :action => 'complete'), :method => 'post' do
+      submit_tag "Complete this Chore!"
+      end
+    else
+    end
+  end
 end
