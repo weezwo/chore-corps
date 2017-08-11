dc6d55f6a05f3ef772d92dbc60311cef50283e97
Luisa Scavo <luisascavo@gmail.com>
2017-08-11 16:45:45 -0400

    add and implement #chores_by_due_date

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,4 +2,8 @@ class Family < ActiveRecord::Base
   has_many :users
   has_many :chores
   has_secure_password
+
+  def chores_by_due_date
+    chores.sort{|chore| calculate_due_date(chore)}
+  end
 end
