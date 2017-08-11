22baaa42cb390170ab3b844a646283b3d6703164
Luisa Scavo <luisascavo@gmail.com>
2017-08-11 10:41:53 -0400

    update poaching process

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,9 @@ class TasksController < ApplicationController
     t.due_date = calculate_due_date(t.chore)
     t.save
     if claimed_chore?(t.chore)
-      t.chore.tasks.where(completion_status: nil).update(completion_status: "poached")
+      t.chore.tasks.where(completion_status: nil).each do |task|
+        task.update(completion_status: "poached")
+      end
     end
     redirect_to chore_path(t.chore)
   end
