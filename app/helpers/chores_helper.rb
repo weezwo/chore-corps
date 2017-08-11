433f374cd2abb0705a75485f6f94e29a455ea1c5
Luisa Scavo <luisascavo@gmail.com>
2017-08-11 17:03:08 -0400

    move relevant chore methods from helpers to model

--- app/helpers/chores_helper.rb
+++ app/helpers/chores_helper.rb
@@ -1,15 +1,4 @@
 module ChoresHelper
-  def cycle_opts
-    [["Daily", "86400"], ["Weekly","604800"], ["Monthly","2592000"], ["Seasonally", "7776000"], ["Yearly","31557600"]]
-  end ## Numbers correspond to the number of seconds that should elapse between tasks
-
-  def chore_frequency
-    pluralize @chore.cycle / 86400, "day"
-  end
-
-  def claimed_chore?(chore)
-    chore.tasks.any?{|task| task.completion_status == 'pending'}
-  end
 
   def user_has_task?(chore)
     current_user.tasks.any?{|task| task.chore_id == chore.id  && task.completion_status == 'pending'}
--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,16 @@
 class Chore < ActiveRecord::Base
   belongs_to :family
   has_many :tasks
+
+  def self.cycle_opts
+    [["Daily", "86400"], ["Weekly","604800"], ["Monthly","2592000"], ["Seasonally", "7776000"], ["Yearly","31557600"]]
+  end ## Numbers correspond to the number of seconds that should elapse between tasks
+
+  def claimed_chore?
+    tasks.any?{|task| task.completion_status == 'pending'}
+  end
+
+  def frequency
+    pluralize self.cycle / 86400, "day"
+  end
 end
