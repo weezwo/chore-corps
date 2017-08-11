2e030f29cc0b45b218e0da6d24f06d629c3eddb0
Luisa Scavo <luisascavo@gmail.com>
2017-08-11 13:03:18 -0400

    update controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,18 +3,22 @@ include ChoresHelper
 class TasksController < ApplicationController
   def create
     t = Task.new(task_params)
-    t.due_date = calculate_due_date(t.chore)
-    t.save
     if claimed_chore?(t.chore)
       t.chore.tasks.where(completion_status: nil).each do |task|
         task.update(completion_status: "poached")
       end
     end
+    t.completion_status = nil
+    t.due_date = calculate_due_date(t.chore)
+    t.save
     redirect_to chore_path(t.chore)
   end
 
   def complete
-    Task.find(params[:id]).update(completion_status: 'complete', completion_date: DateTime.now) 
+    t = Task.find(params[:id])
+    if t.user == current_user
+      t.update(completion_status: 'complete', completion_date: DateTime.now)
+    end
     redirect_to :back
   end
 
