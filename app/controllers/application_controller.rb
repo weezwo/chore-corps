4b2003fd783789dd5db8b801fa9e761b40a91a15
Luisa Scavo <luisascavo@gmail.com>
2017-08-12 22:52:50 -0400

    adjust initializer and app controller to allow custom fields for user sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_family, :calculate_due_date
 
   def current_family
@@ -16,7 +17,8 @@ class ApplicationController < ActionController::Base
     chore.last_completed + chore.cycle
   end
 
+  protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :family => [:name, :password]) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :family => [:name, :password]) }
   end
 end
