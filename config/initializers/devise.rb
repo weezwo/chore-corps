f4933d8d859d57fc752602ad9356c6f3d685d593
Luisa Scavo <luisascavo@gmail.com>
2017-09-08 10:15:51 -0400

    skip jwt in omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
 
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret
+    provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret, skip_jwt: true
   end
 
   # config.omniauth :google_oauth2, client_id: ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
