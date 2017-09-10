f4d60b9dc5a54bc50d3f5c3c234622cee12a599b
Luisa Scavo <luisascavo@gmail.com>
2017-09-09 22:48:21 -0400

    remove console.log in place for testing

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,6 +1,5 @@
 class Task {
   constructor(obj) {
-    console.log(obj)
     this.userName = obj["user"]["name"];
     this.dueDate = Date.parse(obj.due_date);
     this.completionDate = Date.parse(obj.completion_date)
