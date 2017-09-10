b10302042ea27793018ce9ec48df4e05db0092d5
Luisa Scavo <luisascavo@gmail.com>
2017-09-09 22:36:58 -0400

    properly capture userName, add date formatting methods

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,17 @@
 class Task {
   constructor(obj) {
-    this.userName = obj.user.name;
+    console.log(obj)
+    this.userName = obj["user"]["name"];
     this.dueDate = Date.parse(obj.due_date);
     this.completionDate = Date.parse(obj.completion_date)
     this.completionStatus = obj.completion_status;
+  }
+
+  formatDueDate(){
+    return moment(this.dueDate).format('MM-DD-YYYY')
+  }
+
+  formatCompletionDate(){
+    return moment(this.completionDate).format('MM-DD-YYYY')
   }
 }
