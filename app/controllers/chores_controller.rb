4003d3529278fe9a0ae438a29fab976a13500b0a
Luisa Scavo <luisascavo@gmail.com>
2017-09-11 15:19:50 -0400

    ensure inclusion of task.user in index json

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -3,7 +3,7 @@ class ChoresController < ApplicationController
 
   def index
     @family = Family.find(params[:id])
-    render json: @family.chores, each_serializer: FamilyChoreSerializer
+    render json: @family.chores, include: ['tasks.user'], each_serializer: ChoreSerializer
   end
 
   def new
