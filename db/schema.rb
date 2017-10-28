fab9c2cb669286ce7256e08622101df973633348
Luisa Scavo <luisa@Luisas-iMac.local>
2017-10-28 17:27:21 -0400

    create and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170814204856) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chores", force: :cascade do |t|
     t.string   "name"
     t.integer  "family_id"
@@ -59,7 +62,7 @@ ActiveRecord::Schema.define(version: 20170814204856) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
