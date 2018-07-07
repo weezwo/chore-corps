dddefd36037da731f46c82144cd2ab4f70b57995
Luisa Scavo <luisascavo@gmail.com>
2018-07-07 13:41:10 -0400

    add option for biweekly cycle

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -9,7 +9,7 @@ class Chore < ActiveRecord::Base
   end
 
   def self.cycle_opts
-    [["Daily", "86400"], ["Weekly","604800"], ["Monthly","2592000"], ["Seasonally", "7776000"], ["Yearly","31557600"]]
+    [["Daily", "86400"], ["Weekly","604800"], ["Bi-Weekly","1209600"], ["Monthly","2592000"], ["Seasonally", "7776000"], ["Yearly","31557600"]]
   end ## Numbers correspond to the number of seconds that should elapse between tasks
 
   def claimed_chore?
