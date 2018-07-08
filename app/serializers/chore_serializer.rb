6e401cfbd07cfd58431125ca4f36dd7a589fd010
Luisa Scavo <luisascavo@gmail.com>
2018-07-08 16:18:36 -0400

    serialize due_date

--- app/serializers/chore_serializer.rb
+++ app/serializers/chore_serializer.rb
@@ -1,4 +1,4 @@
 class ChoreSerializer < ActiveModel::Serializer
-  attributes :id, :name, :cycle, :last_completed, :notes, :created_at, :updated_at
+  attributes :id, :name, :cycle, :last_completed, :notes, :due_date, :created_at, :updated_at
   has_many :tasks
 end
