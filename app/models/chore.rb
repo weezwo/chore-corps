2222fe99a086c2183a0a40eb6bd9a6fcfebfdfc9
Luisa Scavo <luisascavo@gmail.com>
2018-07-08 16:06:08 -0400

    add cycled/uncycled scopes, validate due date, add one-off options

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,13 +3,22 @@ class Chore < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
+  validates :due_date, absence: {unless: -> {self.cycle.nil?}, message: "must be blank for cycled chores" }
+  validates :due_date, presence: {if: -> {self.cycle.nil?} message: "must be present for one-off chores"}
+
+  scope :uncycled, -> { where(cycle: nil)}
+  scope :cycled, -> { where.not(id: uncycled) }
+
+  def self.by_due_date
+    uncycled + cycled.sort_by{|chore| chore.calculate_due_date}
+  end
 
   def calculate_due_date
-    (self.last_completed || self.created_at) + self.cycle.seconds
+    due_date || (self.last_completed || self.created_at) + self.cycle.seconds
   end
 
   def self.cycle_opts
-    [["Daily", "86400"], ["Weekly","604800"], ["Bi-Weekly","1209600"], ["Monthly","2592000"], ["Seasonally", "7776000"], ["Yearly","31557600"]]
+    [["Daily", "86400"], ["Weekly","604800"], ["Bi-Weekly","1209600"], ["Monthly","2592000"], ["Seasonally", "7776000"], ["Yearly","31557600"], ["One-off", ""]]
   end ## Numbers correspond to the number of seconds that should elapse between tasks
 
   def claimed_chore?
